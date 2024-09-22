e851f34fafd50754fd6fcccdf6e3cc1629b2a129
Nobodip Debnath <nobodipdebnath45@.com>
2024-09-23 01:04:04 +0600

    end dome work

---
+++ js/index.js
@@ -0,0 +1,8 @@
+document.getElementById('blog-btn').addEventListener('click', function(event){
+    event.preventDefault();
+    window.location.href = 'html/blog.html';
+})
+document.getElementById('home-btn').addEventListener('click',function(event){
+    event.preventDefault();
+    window.location.href = '/index.html';
+})
