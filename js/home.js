930acce3f9b725b1ce7d056ae9458413d622bf4e
Nobodip Debnath <nobodipdebnath45@.com>
2024-09-24 22:15:38 +0600

    set button bg color and change the section

--- js/home.js
+++ js/home.js
@@ -6,17 +6,19 @@ document.getElementById('blog-btn').addEventListener('click',function(event){
 
 document.getElementById('donate-btn').addEventListener('click', function(event){
     event.preventDefault();
-    const inputAmount = document.getElementById('input-amount').value;
+    const inputAmount = input('input-amount');
     const amount = parseFloat(inputAmount);
-    if(amount !== 'number' && amount < 0 && inputAmount !== ''){
-        alert('Please try again !');
+    const currentBalance = balance('current-balance');
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    if(isNaN(amount) || amount <= 0 || amount > currentBalanceNumber){
+        alert('Something Wrong . Please try again !');
     }
-    else{
-        const currentBalance = document.getElementById('current-balance').innerText;
-        const donateAmount = document.getElementById('donate-amount').innerText;
+    else{     
+        const donateAmount = balance('donate-amount');
         const newDonateAmount = parseFloat(inputAmount) + parseFloat(donateAmount);  
         document.getElementById('donate-amount').innerText = newDonateAmount;   
-        const newCurrentAmount = parseFloat(currentBalance) - parseFloat(inputAmount);
+        const newCurrentAmount = currentBalanceNumber - parseFloat(inputAmount);
         document.getElementById('current-balance').innerText = newCurrentAmount;
     }
 })
@@ -24,15 +26,17 @@ document.getElementById('donate-btn2').addEventListener('click', function(event)
     event.preventDefault();
     const inputAmount = document.getElementById('input-amount2').value;
     const amount = parseFloat(inputAmount);
-    if(amount !== 'number' && amount < 0 && inputAmount !== ''){
+    const currentBalance = document.getElementById('current-balance').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    if(isNaN(amount) || amount <= 0 || amount > currentBalanceNumber){
         alert('Please try again !');
     }
-    else{
-        const currentBalance = document.getElementById('current-balance').innerText;
+    else{     
         const donateAmount = document.getElementById('donate-amount2').innerText;
         const newDonateAmount = parseFloat(inputAmount) + parseFloat(donateAmount);  
         document.getElementById('donate-amount2').innerText = newDonateAmount;   
-        const newCurrentAmount = parseFloat(currentBalance) - parseFloat(inputAmount);
+        const newCurrentAmount = currentBalanceNumber - parseFloat(inputAmount);
         document.getElementById('current-balance').innerText = newCurrentAmount;
     }
 })
@@ -40,15 +44,17 @@ document.getElementById('donate-btn3').addEventListener('click', function(event)
     event.preventDefault();
     const inputAmount = document.getElementById('input-amount3').value;
     const amount = parseFloat(inputAmount);
-    if(amount !== 'number' && amount < 0 && inputAmount !== ''){
+    const currentBalance = document.getElementById('current-balance').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    if(isNaN(amount) || amount <= 0 || amount > currentBalanceNumber){
         alert('Please try again !');
     }
-    else{
-        const currentBalance = document.getElementById('current-balance').innerText;
+    else{     
         const donateAmount = document.getElementById('donate-amount3').innerText;
         const newDonateAmount = parseFloat(inputAmount) + parseFloat(donateAmount);  
         document.getElementById('donate-amount3').innerText = newDonateAmount;   
-        const newCurrentAmount = parseFloat(currentBalance) - parseFloat(inputAmount);
+        const newCurrentAmount = currentBalanceNumber - parseFloat(inputAmount);
         document.getElementById('current-balance').innerText = newCurrentAmount;
     }
 })
@@ -56,15 +62,17 @@ document.getElementById('donate-btn4').addEventListener('click', function(event)
     event.preventDefault();
     const inputAmount = document.getElementById('input-amount4').value;
     const amount = parseFloat(inputAmount);
-    if(amount !== 'number' && amount < 0 && inputAmount !== ''){
+    const currentBalance = document.getElementById('current-balance').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    if(isNaN(amount) || amount <= 0 || amount > currentBalanceNumber){
         alert('Please try again !');
     }
-    else{
-        const currentBalance = document.getElementById('current-balance').innerText;
+    else{     
         const donateAmount = document.getElementById('donate-amount4').innerText;
         const newDonateAmount = parseFloat(inputAmount) + parseFloat(donateAmount);  
         document.getElementById('donate-amount4').innerText = newDonateAmount;   
-        const newCurrentAmount = parseFloat(currentBalance) - parseFloat(inputAmount);
+        const newCurrentAmount = currentBalanceNumber - parseFloat(inputAmount);
         document.getElementById('current-balance').innerText = newCurrentAmount;
     }
 })
@@ -72,15 +80,17 @@ document.getElementById('donate-btn5').addEventListener('click', function(event)
     event.preventDefault();
     const inputAmount = document.getElementById('input-amount5').value;
     const amount = parseFloat(inputAmount);
-    if(amount !== 'number' && amount < 0 && inputAmount !== ''){
+    const currentBalance = document.getElementById('current-balance').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    if(isNaN(amount) || amount <= 0 || amount > currentBalanceNumber){
         alert('Please try again !');
     }
-    else{
-        const currentBalance = document.getElementById('current-balance').innerText;
+    else{     
         const donateAmount = document.getElementById('donate-amount5').innerText;
         const newDonateAmount = parseFloat(inputAmount) + parseFloat(donateAmount);  
         document.getElementById('donate-amount5').innerText = newDonateAmount;   
-        const newCurrentAmount = parseFloat(currentBalance) - parseFloat(inputAmount);
+        const newCurrentAmount = currentBalanceNumber - parseFloat(inputAmount);
         document.getElementById('current-balance').innerText = newCurrentAmount;
     }
 })
@@ -88,15 +98,42 @@ document.getElementById('donate-btn6').addEventListener('click', function(event)
     event.preventDefault();
     const inputAmount = document.getElementById('input-amount6').value;
     const amount = parseFloat(inputAmount);
-    if(amount !== 'number' && amount < 0 && inputAmount !== ''){
+    const currentBalance = document.getElementById('current-balance').innerText;
+    const currentBalanceNumber = parseFloat(currentBalance);
+
+    if(isNaN(amount) || amount <= 0 || amount > currentBalanceNumber){
         alert('Please try again !');
     }
-    else{
-        const currentBalance = document.getElementById('current-balance').innerText;
+    else{     
         const donateAmount = document.getElementById('donate-amount6').innerText;
         const newDonateAmount = parseFloat(inputAmount) + parseFloat(donateAmount);  
         document.getElementById('donate-amount6').innerText = newDonateAmount;   
-        const newCurrentAmount = parseFloat(currentBalance) - parseFloat(inputAmount);
+        const newCurrentAmount = currentBalanceNumber - parseFloat(inputAmount);
         document.getElementById('current-balance').innerText = newCurrentAmount;
     }
+})
+
+
+document.getElementById('display-history').addEventListener('click', function(){
+
+    document.getElementById('history-section').classList.remove('hidden');
+
+    document.getElementById('donation-section').classList.add('hidden');
+
+    document.getElementById('display-history').classList.add('bg-[#B4F461]');
+
+    document.getElementById('display-donation').classList.remove('bg-[#B4F461]');
+
+
+})
+
+document.getElementById('display-donation').addEventListener('click', function(){
+
+    document.getElementById('donation-section').classList.remove('hidden');
+
+    document.getElementById('history-section').classList.add('hidden');
+
+    document.getElementById('display-history').classList.remove('bg-[#B4F461]');
+
+    document.getElementById('display-donation').classList.add('bg-[#B4F461]');
 })
---
+++ js/library.js
@@ -0,0 +1,8 @@
+function input(id){
+    const inputAmount = document.getElementById(id).value;
+    return inputAmount;
+}
+function balance (id){
+    const currentBalance = document.getElementById(id).innerText;
+    return currentBalance
+}
